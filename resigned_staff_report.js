0ad03dd0f49f1cd1cd1cc26ce0ee197446c7720b
smlfin <senioravp71@gmail.com>
2025-08-26 13:11:37 +0530

    Update resigned_staff_report.js

--- resigned_staff_report.js
+++ resigned_staff_report.js
@@ -1,7 +1,7 @@
 // resigned_staff_report.js
 
 // --- Configuration ---
-const csvUrl = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQ1OOdGnJhw1k6U15Aybn_2JWex_qTShP6w7CXm0_auXnc8vFnvlabPZjK3lsjqkHgn6NgeKKPyu9qW/pub?gid=1720680457&single=true&output=csv';
+const csvUrl = 'https://docs.google.com/spreadsheets/d/1jYlHO8x40Ygbn05DL3tMZ5wHuoZgPjk2fbtEGoDXzko/export?format=csv&gid=1720680457';
 
 // --- Global Data Storage ---
 let allData = []; // Stores all parsed CSV rows
@@ -504,4 +504,5 @@ staffSearchInput.addEventListener('input', () => {
 viewDetailedEntriesBtn.addEventListener('click', viewDetailedEntries);
 
 // --- Initialize the report when the page loads ---
+
 document.addEventListener('DOMContentLoaded', init);
